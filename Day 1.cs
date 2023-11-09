7490f4a51f8ef4f9fb00a446b94a99d1117689dd
Deadly-Pigg <100409468+Deadly-Pigg@users.noreply.github.com>
2023-11-09 19:56:30 +0000

    Update Day 1.cs
    Added comments

--- Day 1.cs
+++ Day 1.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using System.IO;
 
-#region Part 1
+#region Part 1 //This is effectively just finding max
 Console.WriteLine("Day 1 - Part 1:");
-string[] input = File.ReadAllLines("AdventOfCode_Day1.txt");
+string[] input = File.ReadAllLines("AdventOfCode_Day1.txt"); //reads from the file
 List<int> totals = new List<int>();
 int total = 0;
 
 foreach (string calorie in input)
 {
 
-    Console.WriteLine(calorie + ", this is a variable in the input array");
+    Console.WriteLine(calorie + ", this is a variable in the input array"); //debug message
 
-    if (calorie.Length >= 4)
+    if (calorie.Length >= 4) //filters out shorter calories
     { total += int.Parse(calorie.Trim(' ')); }
     else
     {
