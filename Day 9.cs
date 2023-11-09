483cd7bf34d58109b84a59a7db3705e32783b9d8
Deadly-Pigg <100409468+Deadly-Pigg@users.noreply.github.com>
2023-11-09 19:16:05 +0000

    Update Day 9.cs
    added part 2 (the length can be adjusted to make the snake thing longer/shorter)

--- Day 9.cs
+++ Day 9.cs
@@ -1,12 +1,11 @@
 using System.Text.RegularExpressions;
 
-// See https://aka.ms/new-console-template for more information
 string[] file = File.ReadAllLines("AoC9.txt");
 int tempNum = 0;
 int Umax = 0, Rmax = 0, Dmax = 0, Lmax = 0;
 int x = 0, y = 0;
 
-foreach(string line in file)
+foreach (string line in file)
 {
     tempNum = Int32.Parse(Regex.Replace(line, @"[^\d]", ""));
     switch (Regex.Replace(line, @"[\d|\s]", ""))
@@ -24,18 +23,24 @@ foreach(string line in file)
             Lmax += tempNum;
             break;
     }
-    if(x < Math.Abs(Rmax - Lmax)) {x = Math.Abs(Rmax - Lmax);}
-    if(y < Math.Abs(Umax - Dmax)) {y = Math.Abs(Umax - Dmax);}
+    if (x < Math.Abs(Rmax - Lmax)) { x = Math.Abs(Rmax - Lmax); }
+    if (y < Math.Abs(Umax - Dmax)) { y = Math.Abs(Umax - Dmax); }
 }
 
-int moveX = x, moveY = y;
-int oldX = moveX, oldY = moveY;
+int length = 2;
+int[] moveX = new int[length], moveY = new int[length];
+for(int i = 0; i < length; i++)
+{
+    moveX[i] = x;
+    moveY[i] = y;
+}
 x = x * 2 + 10;
 y = y * 2 + 10;
 
-string[,] grid = new string[x,y];
+string[,] grid = new string[x, y];
 int[] xy = new int[2];
 string[,] grid2 = new string[x, y];
+grid[moveX[0], moveY[0]] = "s";
 
 foreach (string line in file)
 {
@@ -43,84 +48,117 @@ foreach (string line in file)
     switch (Regex.Replace(line, @"[\d|\s]", ""))
     {
         case ("U"):
-            for(int i = 0; i<tempNum; i++)
+            for (int i = 0; i < tempNum; i++)
             {
-                grid[moveX, moveY] = ".";
-                moveY--;
-                grid[moveX, moveY] = "H";
-                xy = nearbyCheck(grid, oldX, oldY, moveX, moveY);
-                oldX += xy[0];
-                oldY += xy[1];
-                grid2[oldX, oldY] = "#";
+                grid[moveX[0], moveY[0]] = "";
+                moveY[0]--;
+                grid[moveX[0], moveY[0]] = "H";
+                for (int i2 = 1; i2 < length; i2++)
+                {
+                    xy = nearbyCheck(grid, moveX[i2], moveY[i2], moveX[i2 - 1], moveY[i2 - 1], i2);
+                    moveX[i2] += xy[0];
+                    moveY[i2] += xy[1];
+                    grid[moveX[i2], moveY[i2]] = i2.ToString();
+                    grid2[moveX[length - 1], moveY[length - 1]] = "#";
+                }
+                
             }
             break;
         case ("R"):
             for (int i = 0; i < tempNum; i++)
             {
-                grid[moveX, moveY] = ".";
-                moveX++;
-                grid[moveX, moveY] = "H";
-                xy = nearbyCheck(grid, oldX, oldY, moveX, moveY);
-                oldX += xy[0];
-                oldY += xy[1];
-                grid2[oldX, oldY] = "#";
+                grid[moveX[0], moveY[0]] = "";
+                moveX[0]++;
+                grid[moveX[0], moveY[0]] = "H";
+                for (int i2 = 1; i2 < length; i2++)
+                {
+                    xy = nearbyCheck(grid, moveX[i2], moveY[i2], moveX[i2 - 1], moveY[i2 - 1], i2);
+                    moveX[i2] += xy[0];
+                    moveY[i2] += xy[1];
+                    grid[moveX[i2], moveY[i2]] = i2.ToString();
+                    grid2[moveX[length - 1], moveY[length - 1]] = "#";
+                }
             }
             break;
         case ("D"):
             for (int i = 0; i < tempNum; i++)
             {
-                grid[moveX, moveY] = ".";
-                moveY++;
-                grid[moveX, moveY] = "H";
-                xy = nearbyCheck(grid, oldX, oldY, moveX, moveY);
-                oldX += xy[0];
-                oldY += xy[1];
-                grid2[oldX, oldY] = "#";
+                grid[moveX[0], moveY[0]] = "";
+                moveY[0]++;
+                grid[moveX[0], moveY[0]] = "H";
+                for (int i2 = 1; i2 < length; i2++)
+                {
+                    xy = nearbyCheck(grid, moveX[i2], moveY[i2], moveX[i2 - 1], moveY[i2 - 1], i2);
+                    moveX[i2] += xy[0];
+                    moveY[i2] += xy[1];
+                    grid[moveX[i2], moveY[i2]] = i2.ToString();
+                    grid2[moveX[length - 1], moveY[length - 1]] = "#";
+                }
+                
             }
             break;
         case ("L"):
             for (int i = 0; i < tempNum; i++)
             {
-                grid[moveX, moveY] = ".";
-                moveX--;
-                grid[moveX, moveY] = "H";
-                xy = nearbyCheck(grid, oldX, oldY, moveX, moveY);
-                oldX += xy[0];
-                oldY += xy[1];
-                grid2[oldX, oldY] = "#";
+                grid[moveX[0], moveY[0]] = "";
+                moveX[0]--;
+                grid[moveX[0], moveY[0]] = "H";
+                for (int i2 = 1; i2 < length; i2++)
+                {
+                    xy = nearbyCheck(grid, moveX[i2], moveY[i2], moveX[i2 - 1], moveY[i2 - 1], i2);
+                    moveX[i2] += xy[0];
+                    moveY[i2] += xy[1];
+                    grid[moveX[i2], moveY[i2]] = i2.ToString();
+                    grid2[moveX[length - 1], moveY[length - 1]] = "#";
+                }
+                
+
             }
             break;
     }
 }
 
-Umax = 0;
+int counter = 0;
+Console.WriteLine(x * y);
 foreach (string s in grid2)
 {
-    if (s == "#") { Umax++; }
+    if (s != null) { counter++; }
 }
 
 Console.WriteLine(Umax);
 
-static int[] nearbyCheck(string[,] grid, int oldX, int oldY, int moveX, int moveY)
+for (int ix = 0; ix < x; ix++)
+{
+    for (int iy = 0; iy < y; iy++)
+    {
+        if (grid2[ix, iy] != null)
+        { Console.Write(grid2[ix,iy]); }
+        else { Console.Write("."); }
+    }
+    Console.WriteLine();
+}
+
+
+static int[] nearbyCheck(string[,] grid, int oldX, int oldY, int moveX, int moveY, int n)
 {
     int[] xy = new int[2];
     int val = 0;
     xy[0] = 0;
     xy[1] = 0;
 
-    if (grid[oldX + 1, oldY + 1] == "H") { return xy; }
-    if (grid[oldX + 1, oldY    ] == "H") { return xy; }
-    if (grid[oldX + 1, oldY - 1] == "H") { return xy; }
-    if (grid[oldX,     oldY + 1] == "H") { return xy; }
-    if (grid[oldX,     oldY    ] == "H") { return xy; }
-    if (grid[oldX,     oldY - 1] == "H") { return xy; }
-    if (grid[oldX - 1, oldY + 1] == "H") { return xy; }
-    if (grid[oldX - 1, oldY    ] == "H") { return xy; }
-    if (grid[oldX - 1, oldY - 1] == "H") { return xy; }
+    if (oldX + 1 == moveX && oldY + 1 == moveY) { return xy; }
+    if (oldX + 1 == moveX && oldY == moveY) { return xy; }
+    if (oldX + 1 == moveX && oldY - 1 == moveY) { return xy; }
+    if (oldX == moveX && oldY + 1 == moveY) { return xy; }
+    if (oldX == moveX && oldY == moveY) { return xy; }
+    if (oldX == moveX && oldY - 1 == moveY) { return xy; }
+    if (oldX - 1 == moveX && oldY + 1 == moveY) { return xy; }
+    if (oldX - 1 == moveX && oldY == moveY) { return xy; }
+    if (oldX - 1 == moveX && oldY - 1 == moveY) { return xy; }
 
 
-    val = (moveX - oldX) == 0 ? xy[0] = 0 : val = (moveX-oldX) > 0 ? xy[0] = 1 : xy[0] = -1;
-    val = (moveY - oldY) == 0 ? xy[1] = 0 : val = (moveY-oldY) > 0 ? xy[1] = 1 : xy[1] = -1;
+    val = (moveX - oldX) == 0 ? xy[0] = 0 : val = (moveX - oldX) > 0 ? xy[0] = 1 : xy[0] = -1;
+    val = (moveY - oldY) == 0 ? xy[1] = 0 : val = (moveY - oldY) > 0 ? xy[1] = 1 : xy[1] = -1;
 
     return xy;
 }
