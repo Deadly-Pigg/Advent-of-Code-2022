d15ed6fbe7d863ae17501eb66f7e4c53b039092e
Deadly-Pigg <100409468+Deadly-Pigg@users.noreply.github.com>
2023-11-09 20:14:00 +0000

    day 4
    day 4

--- Program.cs
+++ Program.cs
@@ -1,77 +1,23 @@
-﻿using System.Text.RegularExpressions;
-using System.Collections.Generic;
-
-string[] file = File.ReadAllLines("AoC5.txt"); //Declaration of variables
-string[] file2 = File.ReadAllLines("AoC5(2).txt");
-int c = 1;
-string text = "";
-string[] move = new string[3];
-int listval = 0;
-
-List<string>[] list = new List<string>[30];//Declares a List Array
-for (int i = 0; i < list.Length; i++)
+﻿string[] input = File.ReadAllLines("Aoc4.txt");
+string[] num = new string[4];
+int i = 0;
+int val = 0;
+for(i = 0; i < input.Length; i++)
 {
-    list[i] = new List<string>();
+    num =  input[i].Split('-', ',');
+    /* if (Convert.ToInt32(num[0]) <= Convert.ToInt32(num[2]) && Convert.ToInt32(num[1]) >= Convert.ToInt32(num[3]))
+    {
+        val++;
+    }
+    else if (Convert.ToInt32(num[2]) <= Convert.ToInt32(num[0]) && Convert.ToInt32(num[3]) >= Convert.ToInt32(num[1]))
+    {
+        val++;
+    }    */ //Part 1
+    Console.WriteLine(input[i]);
+    if (Convert.ToInt32(num[1]) >= Convert.ToInt32(num[2]) && Convert.ToInt32(num[0]) <= Convert.ToInt32(num[3]))
+    {
+        val++;
+    }
 }
 
-foreach (string s in file2)
-{
-    for (int i = 0; i < (list.Length*2-1); i += 2)
-    {
-        if (s.Substring(i, 1) != " ")
-        {
-            list[i / 2].Add(s.Substring(i, 1));
-        }
-    }
-    c++;
-} //Stores the values from the file into the list array (file2 contents)
-
-c = 0;
-foreach(List<string> l in list) 
-{
-    list[c].Reverse();
-    c++;
-
-} //Reverses the list, and also counts how many values are in every list, for whatever reason
-Console.WriteLine(listval);
-listval = 0;
-
-foreach (string s in file)
-{
-    Console.WriteLine("-----------------------------------------------------");
-    text = (Regex.Replace(s, @"[a-z]", "")).TrimStart(' ');
-    move = text.Split("  ");
-    text = "";
-    for (int i = 0; i < Convert.ToInt32(move[0]); i++)
-    {
-        foreach (string f in list[Convert.ToInt32(move[1]) - 1])
-        {
-            listval++;
-        }//counts the amount of elements in the list
-        text += list[Convert.ToInt32(move[1]) - 1].Last();
-        list[Convert.ToInt32(move[1]) - 1].RemoveAt(listval-1);
-        listval = 0;
-    }//Moves crates around
-
-    char[] array = text.ToCharArray(); //Part 2 code; appends the position of the crates, but does it for multiple at once
-    Array.Reverse(array); // Yeah, Part 2's code is incredibly easy to code in bruh
-    text = new string(array);
-
-    Console.WriteLine(text);
-    foreach(char ch in text)
-    {
-        list[Convert.ToInt32(move[2]) - 1].Add(Convert.ToString(ch));
-    }
-    foreach (List<string> l in list)
-    {
-        foreach (string s2 in l)
-        { Console.Write("[" + s2 + "]"); }
-        Console.WriteLine();
-    }//Outputting the crates; doesn't do much, just displays it for debugging
-} //Part 1 code; appends the position of the crates (1 crate at a time) also includes Part 2 code
-bool check = false;
-
-foreach (List<string> l in list)
-{
-        Console.Write(l.Last());
-} //Outputs the final solution because I'm lazy and can't add it up myself
+Console.WriteLine(val);
